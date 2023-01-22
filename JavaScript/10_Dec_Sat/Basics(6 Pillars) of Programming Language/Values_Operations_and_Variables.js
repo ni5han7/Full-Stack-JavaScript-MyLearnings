7a62daf2a3512eae8f6dd8b7c1dfe8e087726af2
Nishant Jaiswal <ni5han7jaiswal@gmail.com>
2023-01-23 01:20:51 +0530

    JavaScript files added.

---
+++ JavaScript/10_Dec_Sat/Basics(6 Pillars) of Programming Language/Values_Operations_and_Variables.js
@@ -0,0 +1,63 @@
+//Primitive Values or Primitive Data Types(have one single value) 
+
+//Four types of Primitive Values:
+
+//1st. Number
+// 0
+// 6
+// 1.2
+// 0.5566
+
+//2nd. String
+// "Nishant"
+// 'Nishant'
+
+//3rd. Boolean Values
+// true
+// false
+
+
+//4th. Empty Values
+// null
+// undefined
+
+//User defined Data Types(they contain multiple values that may be of different types)
+//1st. Array 
+// ['Nishant', 1, 2, 3.5, true];
+//  0.......1..2..3......4   //index
+
+//2nd. Object
+// {
+//     key:value,
+//     fname:'Nishant',
+//     lname:'Jaiswal'
+// }
+
+
+//Place holder for Values
+
+// var x =10;
+// let y = 22;
+// const z ='Nishant';
+
+let fname = 'Nishant';
+let lname = 'Jaiswal';
+
+console.log(fname, lname);
+console.log('My firstname is', fname);
+console.log('My last name is', lname);
+
+//Template Literal (`)
+
+console.log(`My firstanme and lastname is ${fname} ${lname}`);
+
+//Operators
+//Arithmetic Operators
+//+,-,*,/,%
+//Comparison Operators
+//==(Compares Values) and ===(Compares both Values and their Data Types)
+
+
+//Conditions/Decisions
+//if/else
+//To be continued... 
